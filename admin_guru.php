3ed765ca6b6a540b9b72295d7631ef3cb6a0e95b
Iven Lagowan <94290469+ivendf@users.noreply.github.com>
2022-10-13 21:11:56 -0700

    usert

---
+++ admin_guru.php
@@ -0,0 +1,25 @@
+<?php
+include("inc_header.php");
+if (!in_array("guru", $_SESSION['admin_akses'])) {
+    echo "Kamu tidak punya akses";
+    include("inc_footer.php");
+    exit();
+}
+?>
+<h1>Halaman Guru</h1>
+Selamat datang di halaman guru
+<?php
+include("inc_footer.php");
+?><?php
+include("inc_header.php");
+if (!in_array("siswa", $_SESSION['admin_akses'])) {
+    echo "Kamu tidak punya akses";
+    include("inc_footer.php");
+    exit();
+}
+?>
+<h1>Halaman Siswa</h1>
+Selamat datang di halaman siswa
+<?php
+include("inc_footer.php");
+?>
